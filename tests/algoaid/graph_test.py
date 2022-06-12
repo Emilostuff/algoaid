7a7c82e60e8e5a4d96c706d2614561e2fd6dd731
Emilostuff <emilostuff@gmail.com>
2022-06-12 18:34:38 +0200

    added more tests

--- tests/algoaid/graph_test.py
+++ tests/algoaid/graph_test.py
@@ -31,8 +31,26 @@ def test_get_edges_digraph():
     assert len(edges) == len(graph_edges)
 
     for (a, b, w) in edges:
-        # print([f"{e.start.key}..{e.to.key} {e.weight}" for e in graph_edges])
         assert any(
             [e.start.key == str(a) and e.to.key == str(
                 b) and e.weight == w for e in graph_edges]
+        )
+
+
+def test_get_edges_graph():
+    # construct graph
+    g = Graph(EDGES_WEIGHTS, type=Graph.GraphType.GRAPH)
+
+    edges = [(0, 2, 6), (0, 1, 1), (0, 5, 2), (0, 4, 1),
+             (1, 2, 8), (4, 8, 7), (8, 5, 3)]
+    graph_edges = g.get_edges()
+    assert len(edges) == len(graph_edges)
+
+    for (a, b, w) in edges:
+        assert any(
+            [e.start.key == str(a) and e.to.key == str(
+                b) and e.weight == w for e in graph_edges]
+        ) or any(
+            [e.start.key == str(b) and e.to.key == str(
+                a) and e.weight == w for e in graph_edges]
         )
